cdcbc7047ed76ff394017c9db42a43f324319586
Dong Yeop-Lee <33149488+hjyj4841@users.noreply.github.com>
2026-05-18 23:46:53 +0900

    first commit
    프로젝트 생성

---
+++ listenup/src/main/java/com/listenup/listenup/ListenupApplication.java
@@ -0,0 +1,13 @@
+package com.listenup.listenup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListenupApplication.class, args);
+	}
+
+}
